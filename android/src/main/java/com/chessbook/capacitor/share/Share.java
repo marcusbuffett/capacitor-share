280a1c08fb051bc486f09ccafa6d53035981acea
Marcus Buffett <me@mbuffett.com>
2024-04-30 09:30:42 -0600

    Initial commit

---
+++ android/src/main/java/com/chessbook/capacitor/share/Share.java
@@ -0,0 +1,11 @@
+package com.chessbook.capacitor.share;
+
+import android.util.Log;
+
+public class Share {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
---
+++ android/src/main/java/com/chessbook/capacitor/share/SharePlugin.java
@@ -0,0 +1,22 @@
+package com.chessbook.capacitor.share;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "Share")
+public class SharePlugin extends Plugin {
+
+    private Share implementation = new Share();
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+}
